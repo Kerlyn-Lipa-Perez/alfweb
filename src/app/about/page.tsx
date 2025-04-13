fc4bd5183449fa0c09ce155def2159ffd6294e9c
Kerlyn Lipa <kerlynlipaperez@gmail.com>
2025-04-12 19:49:37 -0500

    Initial commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function About() {
+  return (
+    <>
+    <div>
+        <h1>
+            About Page
+        </h1>
+        <section>
+            <h2>About Us</h2>
+            <p></p>
+        </section>
+    </div>
+
+    </>
+  )
+}
+
+export default About
