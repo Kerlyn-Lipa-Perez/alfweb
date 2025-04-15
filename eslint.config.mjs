97238a4e231dc384bc7dda5c49f2228ff8b6ca94
Kerlyn Lipa <kerlynlipaperez@gmail.com>
2025-04-14 22:43:53 -0500

    Added eslint plugin and hero component

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import perfectionist from "eslint-plugin-perfectionist";
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +12,26 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      perfectionist,
+    },
+    rules: {
+      "perfectionist/sort-interfaces": ["error"],
+      "perfectionist/sort-objects": [
+        "error",
+        {
+          type: "alphabetical",
+        },
+      ],
+    },
+    settings: {
+      perfectionist: {
+        partitionByComment: true,
+        type: "line-length",
+      },
+    },
+  },
 ];
 
 export default eslintConfig;
